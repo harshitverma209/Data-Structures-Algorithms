d030ebf6d76fc0c78f955813db98e584af89c1c7
Harshit Verma <33396779+harshitverma209@users.noreply.github.com>
2021-09-14 13:35:01 +0530

    Added support for unsorted nests

--- Angry Birds.cpp
+++ Angry Birds.cpp
@@ -2,6 +2,8 @@
 the minimum distance between any two birds is the largest*/
 #include<iostream>
 #include<vector>
+#include<algorithm>
+
 using namespace std;
 bool can_fit(vector<int> nests,int birds,int min_dis){
     int prev_nest=nests[0];
@@ -38,6 +40,7 @@ int main(){
     int birds=3;
     int least_distance=0;
     int max_distance=nests[nests.size()-1]-nests[0];
+    sort(nests.begin(),nests.end());
     int res=solve_angry_birds(nests,birds,least_distance,max_distance);
     cout<<res<<endl;
     return 0;
